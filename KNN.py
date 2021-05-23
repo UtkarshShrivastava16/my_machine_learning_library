20e216463b22fa972c64d849ffe6ab28f52dde9f
Utkarsh Shrivastava <34977336+UtkarshShrivastava16@users.noreply.github.com>
2021-05-23 11:02:21 +0530

    Logistic Regression

---
+++ KNN.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 10:55:06 2021
+
+@author: hp
+"""
+
--- LogisticRegression.py
+++ LogisticRegression.py
@@ -26,13 +26,13 @@ class LogisticRegression:
         h = 1/(1+np.exp(z)) 
         return h
     
-    def predict(self, theta, X):
-        y_pred = self.hypothesis_function(theta, X)
-        return (y_pred>0.5).astype(int)
+    def predict(self, X, threshold = 0.5):
+        probability = self.hypothesis_function(self.w, X)
+        print(probability)
+        return (probability<threshold).astype(int)
         
         
     def gradient_descent(self):
-        
         X = self.X
         y = self.y
         y = y.reshape(len(y),1)
@@ -46,13 +46,11 @@ class LogisticRegression:
            
             h = self.hypothesis_function(theta, X)
             # print("h", h.shape)
-            # print("y", y.shape)
-            temp1 = h-y
+            # print("y", y.shape)          
             # print("h-y ", temp1.shape)
-            
-            temp = np.matmul(X.T,temp1)
+            dtheta = (alpha/m)*np.matmul(X.T,h-y)
             # print("temp ", temp.shape)
-            theta = np.subtract(theta , (alpha/m)*temp)
+            theta = np.subtract(theta , dtheta)
         # print(theta.shape)
         return theta
     
@@ -79,7 +77,7 @@ X_test = sc.transform(X_test)
 # train
 
 classifier = LogisticRegression(X_train, y_train)
-y_pred = classifier.predict(classifier.w, X_test)
+y_pred = classifier.predict(X_test)
 
 #%%
 
@@ -88,3 +86,22 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 print(accuracy_score(y_test, y_pred))
+#%%
+
+## Visualising the Training set results
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = sc.inverse_transform(X_train), y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
